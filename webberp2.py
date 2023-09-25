182c2c343b1286db8a72bfadf6a67f0a703827fb
Ezeabasili Edward <81073568+EDwith2ds@users.noreply.github.com>
2023-09-25 11:07:13 +0100

    ttrying to fix error on line 32

--- webberp2.py
+++ webberp2.py
@@ -3,8 +3,6 @@ from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
-# animename = str(input())
-
 browser = Firefox()
 browser_action = ActionChains(browser)
 browser.get("https://animepahe.ru")
@@ -14,7 +12,7 @@ time.sleep(3)
 def pahe_search():
     animepahe_search = browser.find_element(By.CSS_SELECTOR, ".input-search")
     animepahe_search.click()
-    animepahe_search.send_keys('jobless reincarnation')
+    animepahe_search.send_keys("jobless reincarnation")
     time.sleep(1)
     anime = browser.find_element(By.CSS_SELECTOR, ".search-results > li:nth-child(1) > a:nth-child(1)")
     anime.click()
@@ -39,47 +37,48 @@ except:
         browser_action.scroll_to_element(pahe_episode).perform()
         pahe_episode.click()
         time.sleep(1)
+    else:
+        browser_action.scroll_to_element(pahe_episode)
+        time.sleep(1)
+        pahe_episode.click()
 
-def find_ele(selector=str()):
-    browser. find_element(By.CSS_SELECTOR, selector)
-    browser.click()
-
+download = browser.find_element(By.CSS_SELECTOR, '#downloadMenu')
+p1080 = browser.find_element(By.CSS_SELECTOR, "#pickDownload > a:nth-child(3)")
 try:
-    find_ele(selector='#downloadMenu')
+    download.click()
 except:
     if browser.current_window_handle == 0:
-        find_ele('#downloadMenu')
+        download.click()
+        time.sleep(1)
+        p1080.click()
     else:
-        browser.switch_to(browser.window_handles[0])
-        find_ele('#downloadMenu')
+        browser.switch_to.browser.window_handles[0]
+        download.click()
+        if browser.current_window_handle != 0:
+            browser.switch_to.browser.window_handles[0]
+            p1080.click()
+        else: p1080.click
 
 try:
-    find_ele()
+    browser
 except:
     if browser.current_window_handle == 0:
-        find_ele()
+        browser
     else:
-        find_ele()
+                
 
 # > stage 2: Navigate the browser to the download page for pahewin
+
 try:
-    find_ele()
+    browser.find_element(By.CSS_SELECTOR)
 except:
     browser.switch_to.window(browser.window_handle[1])
     time.sleep(6)
-    find_ele()
+    
 
 # > stage 3: navigate the browser to the download page of kwik
 try:
-    find_ele()
+    browser
 except:
     browser.switch_to.window(browser.window_handle[1])
-    find_ele()
-
-# 
-browser.current_url
-browser.window_handles
-browser.get_window_position()
-browser.set_page_load_timeout(10)
-browser.current_url
-browser.window_handles
+    
